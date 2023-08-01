08da0bf0b3990d6603a4c2b7122ec8b92980c9f4
mlc1503 <manelagunas1503@gmail.com>
2023-08-01 10:57:24 +0200

    obras en tuTab.html. parte 1 de ¿
    Falta estilizar y rellenar con informacion la vista

---
+++ Views/tuTab.js
@@ -0,0 +1,45 @@
+$(function(){
+    tabShown(1);
+})
+
+function tabShown(tabID){
+    //shows clicked tab and hides all others
+    if(tabID == 1){
+        $("#datos").show()
+        $("#photos").hide()
+        $("#observations").hide()
+        $("#publications").hide()
+    }
+    else if(tabID == 2){
+        $("#datos").hide()
+        $("#photos").show()
+        $("#observations").hide()
+        $("#publications").hide()
+    }
+    else if(tabID == 3){
+        $("#datos").hide()
+        $("#photos").hide()
+        $("#observations").show()
+        $("#publications").hide()
+    }
+    else if(tabID == 4){
+        $("#datos").hide()
+        $("#photos").hide()
+        $("#observations").hide()
+        $("#publications").show()
+    }
+}
+
+function createObservation(){
+    //TODO: either discrete view or modal window
+    window.location.href = "createObservation.html";
+}
+function createPublication(){ //???? what is the final use of this?
+    
+    //TODO: either discrete view or modal window
+    window.location.href = "createObservation.html";
+}
+function uploadPhoto(){
+    //TODO: either discrete view or modal window
+    window.location.href = "createObservation.html";
+}
